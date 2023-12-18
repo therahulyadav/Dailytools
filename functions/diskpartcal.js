43d9b06709d8968bba901eb7b1c5891f9f9b8f45
Rahul Yadav <therahulyadav@users.noreply.github.com>
2023-12-19 01:53:47 +0530

    add boiler js

---
+++ functions/diskpartcal.js
@@ -0,0 +1,8 @@
+let size = document.querySelector("#size");
+const button = document.querySelector("#btn");
+button.addEventListener('click',()=>{
+    let sizes = parseInt(size.value);
+    console.log(sizes);
+    let fat32 ;
+    let ntfs;
+})
